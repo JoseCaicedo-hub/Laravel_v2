28c6cff6eb9d257e4567f9ea7abc012e56a4a8ec
DanielFonseca117 <danifon44@email.com>
2025-09-22 14:49:56 -0500

    New

---
+++ resources/views/plantilla/header.blade.php
@@ -0,0 +1,55 @@
+ <nav class="app-header navbar navbar-expand bg-body">
+     <!--begin::Container-->
+     <div class="container-fluid">
+         <!--begin::Start Navbar Links-->
+         <ul class="navbar-nav">
+             <li class="nav-item">
+                 <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
+                     <i class="bi bi-list"></i>
+                 </a>
+             </li>
+             <li class="nav-item d-none d-md-block"><a href="#" class="nav-link">Home</a></li>
+             <li class="nav-item d-none d-md-block"><a href="#" class="nav-link">Contact</a></li>
+         </ul>
+         <!--end::Start Navbar Links-->
+         <ul class="navbar-nav ms-auto">
+             <!--begin::Fullscreen Toggle-->
+             <li class="nav-item">
+                 <a class="nav-link" href="#" data-lte-toggle="fullscreen">
+                     <i data-lte-icon="maximize" class="bi bi-arrows-fullscreen"></i>
+                     <i data-lte-icon="minimize" class="bi bi-fullscreen-exit" style="display: none"></i>
+                 </a>
+             </li>
+             <!--end::Fullscreen Toggle-->
+             <!--begin::User Menu Dropdown-->
+             @if (Auth::check())                         
+             <li class="nav-item dropdown user-menu">
+                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                     <span class="d-none d-md-inline"> {{ Auth::user()->name }}  </span>
+                 </a>
+                 <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
+                     <!--begin::User Image-->
+                     <li class="user-header text-bg-primary">
+                         <p>
+                             {{ Auth::user()->name }}
+                         </p>
+                     </li>
+                     <!--end::User Image-->
+
+                     <!--begin::Menu Footer-->
+                     <li class="user-footer">                      
+                         <a href="#" onclick="document.getElementById('logout-form').submit();" class="btn btn-default btn-flat float-end">Cerrar sesión</a>
+                     </li>
+                     <!--end::Menu Footer-->
+                 </ul>
+             </li>
+             <form action="{{ route('logout') }}" id="logout-form" method="post" class="d-none" >
+                @csrf
+             </form>
+             @endif
+             <!--end::User Menu Dropdown-->
+         </ul>
+         <!--end::End Navbar Links-->
+     </div>
+     <!--end::Container-->
+ </nav>
---
+++ resources/views/usuario/index.blade.php
@@ -0,0 +1,104 @@
+@extends('plantilla.app')
+@section('contenido')
+    <div class="app-content">
+        <!--begin::Container-->
+        <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <h3 class="card-title">Usuarios</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <div>
+                                <form action="" method="get">
+                                    <div class="input-group">
+                                        <input name="texto" type="text" class="form-control" value=""
+                                            placeholder="Ingrese texto a buscar">
+                                        <div class="input-group-append">
+                                            <button type="submit" class="btn btn-primary">
+                                                <i class="fas fa-search"></i>
+                                                Buscar</button>
+                                            <a href="{{ route('usuarios.create') }}" class="btn btn-primary">Nuevo</a>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            @if (Session::has('mensaje'))
+                                <div class="">
+                                    {{ Session::get('mensaje') }}
+                                </div>
+                            @endif
+                            <div class="table-responsive mt-3">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th style="width: 130px">Opciones</th>
+                                            <th style="width: 20px">ID</th>
+                                            <th>Nombre</th>
+                                            <th>Correo</th>
+                                            <th>Activo</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @if (count($registros) <= 0)
+                                            <tr>
+                                                <td colspan="5">No hay registros que coincidan con la busqueda</td>
+                                            </tr>
+                                        @else
+                                            @foreach ($registros as $reg)
+                                                <tr>
+                                                    <td>
+                                                        <a href="{{ route('usuarios.edit', $reg->id) }}"
+                                                            class="btn btn-info btn-sm">
+                                                            <i class="bi bi-pencil-fill"></i>
+                                                        </a>
+                                                        <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                                                            data-bs-target="#modal-eliminar-{{ $reg->id }}">
+                                                            <i class="bi bi-trash-fill"></i>
+                                                        </button>
+                                                        <button
+                                                            class="btn {{ $reg->activo ? 'btn-success' : 'btn-warning' }}  btn-sm"
+                                                            data-bs-toggle="modal"
+                                                            data-bs-target="#modal-toggle-{{ $reg->id }}">
+                                                            <i
+                                                                class="bi {{ $reg->activo ? 'bi-check-circle' : 'bi-ban' }} "></i>
+                                                        </button>
+                                                    </td>
+                                                    <td>{{ $reg->id }}</td>
+                                                    <td>{{ $reg->name }}</td>
+                                                    <td>{{ $reg->email }}</td>
+                                                    <td>
+                                                        <span class="badge {{ $reg->activo ? 'bg-success' : 'bg-danger' }}">
+                                                            {{ $reg->activo ? 'Activo' :'Inactivo' }}
+                                                        </span>
+
+                                                    </td>
+                                                </tr>
+                                                @include('usuario.delete')
+                                                @include('usuario.activate')
+                                            @endforeach
+                                        @endif
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer clearfix">
+                            {{ $registros->appends(['texto' => $texto]) }}
+                        </div>
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
+
+            </div>
+            <!--end::Row-->
+        </div>
+        <!--end::Container-->
+    </div>
+@endsection
